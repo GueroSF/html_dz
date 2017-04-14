6deb0b16e82aefaf1aa0eb81b803b173863b9aae
Gosha <guerosf@yandex.ru>
2017-04-14 12:56:49 +0300

    убрал console.log

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function magic(){
 	let scrollV = window.pageYOffset;
 	let width = document.documentElement.clientWidth
 	let leftMenu = document.querySelector('.left_menu');
-	console.log(scrollV,document.documentElement.clientWidth);
 	if (scrollV>=75&&width<=600){
 		document.querySelector('content').style.marginTop = '62px';
 		leftMenu.classList.add('top_menu')
